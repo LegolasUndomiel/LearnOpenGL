6768dc02dc8c2d437428941d02b59b647c02abcf
王松 <l.undomiel@outlook.com>
2024-10-03 11:10:27 +0800

    OpenGL #6

--- src/Triangle/Triangle.cc
+++ src/Triangle/Triangle.cc
@@ -1,7 +1,54 @@
-#include <glad/glad.h>
+// #include <glad/glad.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdlib.h>
+
+static unsigned int CompileShader(unsigned int type,
+                                  const std::string &source) {
+    unsigned int id = glCreateShader(type);
+    const char *src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // Error handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char *message = (char *)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile "
+                  << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+                  << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string &vertexShader,
+                                 const std::string &fragmentShader) {
+    unsigned int program = glCreateProgram();
+    // Compile shader
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    // Link shader
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    // Delete shader
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(int argc, char const *argv[]) {
     // Initialize GLFW
@@ -30,11 +77,16 @@ int main(int argc, char const *argv[]) {
     // Make the window's context current
     glfwMakeContextCurrent(window);
 
+#if 0
     // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         exit(EXIT_FAILURE);
     }
+#else
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+#endif
 
     // print version information
     // 必须在glad初始化之后才能调用
@@ -51,11 +103,41 @@ int main(int argc, char const *argv[]) {
 
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    // enable
+    glEnableVertexAttribArray(0);
+    // layout
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader = "#version 460 core\n"
+                               "\n"
+                               "layout(location = 0) in vec4 position;\n"
+                               "\n"
+                               "void main()\n"
+                               "{\n"
+                               "    gl_Position = position;\n"
+                               "}\n";
+
+    std::string fragmentShader =
+        "#version 460 core\n"
+        "\n"
+        "layout(location = 0) out vec4 FragColor;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     // render loop
     while (!glfwWindowShouldClose(window)) {
         // clear the screen
         glClearColor(100.0f / 255, 149.0f / 255, 237.0f / 255, 0.8f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // swap buffers
         glfwSwapBuffers(window);
@@ -63,6 +145,7 @@ int main(int argc, char const *argv[]) {
         // poll events
         glfwPollEvents();
     }
+    glfwTerminate();
 
     return 0;
 }
